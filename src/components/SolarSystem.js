0c94a0d2c973daa43d36adbe267ba9accec5fdb5
severo <oliveira.severo@gmail.com>
2021-10-29 18:28:54 -0300

    Erros de hidentacao no arquivo solarsystem que passaram

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class SolarSystem extends Component {
-render() {
-  return (
-    <div data-testid="solar-system">
+  render() {
+    return (
+      <div data-testid="solar-system">
         <p>lolfdjfidjfidj</p>
-    </div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 export default SolarSystem;
