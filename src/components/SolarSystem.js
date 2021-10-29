455f16d589f8e74e14b68f9364882a16a2817df4
severo <oliveira.severo@gmail.com>
2021-10-29 17:09:19 -0300

    Criando o componente SolarSystem e chamando ele em app.js - conectado com conteudo Lorem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <header>
+        <h1>SolarSystem Component</h1>
+      </header>
+    );
+  }
+}
+
+export default SolarSystem;
