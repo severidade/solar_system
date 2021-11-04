71adf4fad230fba0dcd97dffe06b1da569266e68
severo <oliveira.severo@gmail.com>
2021-11-04 00:36:07 -0300

    criando uma classe planet-card para os planetas e definindo as propriedades flex para elas

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,7 +6,7 @@ class PlanetCard extends Component {
     const { planetName } = this.props;
     const { planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div className="planet-card" data-testid="planet-card">
         <h3 data-testid="planet-name">{planetName}</h3>
         <figure className="conteiner_planet_image">
           <img src={ planetImage } alt={ `Planeta ${planetName}` } />
