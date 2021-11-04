b9f4b15dea4cd3094a835c5fc8acafdcf21262a2
severo <oliveira.severo@gmail.com>
2021-11-04 12:39:24 -0300

    Renderizando o componente  dentro do componente

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
       <div className="conteiner_missions" data-testid="missions">
+        <Title headline="Missões" />
         <p>Aqui entram as missões </p>
       </div>
     );
