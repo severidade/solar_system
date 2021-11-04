9ca507889797a73518e841b90fb5bf2803d70d3b
severo <oliveira.severo@gmail.com>
2021-11-04 09:03:30 -0300

    Criacao do componente Missions e ajustes ao css para centralizar o conteudo

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div className="conteiner_missions" data-testid="missions">
+        <p>Aqui entram as missões </p>
+      </div>
+    );
+  }
+}
+
+export default Missions;
