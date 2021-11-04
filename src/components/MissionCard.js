37ba6cf5746bcd20fd23f07c2f9aa7ec64bd861a
severo <oliveira.severo@gmail.com>
2021-11-04 14:29:29 -0300

    Erros de lint

---
+++ src/components/MissionCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class MissionCard extends Component {
+  render() {
+    // - `name`
+    // - `year`
+    // - `country`
+    // - `destination`
+    // const { planetName } = this.props;
+    // const { planetImage } = this.props;
+    return (
+      <div>
+        <p></p>
+      </div>
+    );
+  }
+}
+
+export default MissionCard;
